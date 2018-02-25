d49c09d060910a43af14d7032af95c36f1ed5423
Rafael Monteiro <rafaelmonteiro.developer@gmail.com>
2018-02-25 15:15:55 -0300

    Consumindo o serviço de listagem da API

--- patrimonio-ui/src/app/app.module.ts
+++ patrimonio-ui/src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { DataTableModule } from 'primeng/primeng';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { ItemCadastroComponent } from './item-cadastro/item-cadastro.component';
+
+import { ItemService } from './item.service';
 
 
 @NgModule({
@@ -16,9 +19,12 @@ import { ItemCadastroComponent } from './item-cadastro/item-cadastro.component';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     DataTableModule
   ],
-  providers: [],
+  providers: [
+    ItemService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- patrimonio-ui/src/app/item-cadastro/item-cadastro.component.ts
+++ patrimonio-ui/src/app/item-cadastro/item-cadastro.component.ts
@@ -1,15 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ItemService } from '../item.service';
 
 @Component({
   selector: 'app-item-cadastro',
   templateUrl: './item-cadastro.component.html',
   styleUrls: ['./item-cadastro.component.css']
 })
-export class ItemCadastroComponent  {
+export class ItemCadastroComponent implements OnInit  {
 
-  itens = [
-    { etiqueta : 'AA0001', descricao : 'Notebook Dell i5', dataAquisicao : new Date() },
-    { etiqueta : 'AA0034', descricao : 'Monitor Led 20', dataAquisicao : new Date() }
-  ]
+  itens = [];
+
+  constructor(private itemService : ItemService) { }
+
+  ngOnInit() {
+    this.listar();
+  }
+
+  listar() {
+    this.itemService.listar()
+            .subscribe(itens => this.itens = itens);
+  }
 
 }
